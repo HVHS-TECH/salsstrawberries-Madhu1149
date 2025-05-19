2901491e139b444dd8dc574aff97be1869679d60
Madhu1149 <23477mt@hvhs.school.nz>
2025-05-19 00:31:39 +0000

    initilise

--- main.mjs
+++ main.mjs
@@ -1,1 +1,29 @@
+/**************************************************************/
+// main.mjs
+// Main entry for index.html
+// Written by <Madhulika Tirindi>, Term 2 2025
+/**************************************************************/
 
+ 
+
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the constants & functions required from script.mjs
+import { fb_initialise }
+    from './script.mjs';
+    window.fb_initialise   = fb_initialise;
+
+import { fb_authenticate }
+    from './script.mjs';
+    window.fb_authenticate   = fb_authenticate;
+
+
+/**************************************************************/
+// index.html main code
+/**************************************************************/
+
+
+/**************************************************************/
+//   END OF CODE
+/**************************************************************/
--- script.mjs
+++ script.mjs
@@ -1,1 +1,73 @@
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the methods you want to call from the firebase modules
+ alert( onclick=fb_initialise ());
 
+
+ 
+
+
+import { initializeApp }    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase}  from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+/**************************************************************/
+//Firevase Configuration
+/**************************************************************/
+const FB_GAMECONFIG = {
+    apiKey: "AIzaSyAVR1PCSvRfuJtVBBaTOS_Q1l6M9Si0zwk",
+    authDomain: "comp-2025-tirindi-madhulika.firebaseapp.com",
+    databaseURL: "https://comp-2025-tirindi-madhulika-default-rtdb.firebaseio.com",
+    projectId: "comp-2025-tirindi-madhulika",
+    storageBucket: "comp-2025-tirindi-madhulika.firebasestorage.app",
+    messagingSenderId: "492713596983",
+    appId: "1:492713596983:web:1a462d99a51bd1cef607b2",
+    measurementId: "G-RNYJ2GKKLL"
+  };
+/**************************************************************/
+// EXPORT FUNCTIONS
+/**************************************************************/
+export { 
+    fb_initialise };
+
+export { 
+    fb_authenticate };
+
+
+
+/**************************************************************/
+// EXPORT FUNCTIONS
+// List all the functions called by code or html outside of this module
+/**************************************************************/
+
+function fb_initialise() {
+    console.log('%c fb_initialise(): ', 
+                
+    
+                const FB_GAMEAPP = initializeApp(FB_GAMECONFIG);    
+                const FB_GAMEDB = getDatabase(FB_GAMEAPP);           
+                console.info(FB_GAMEDB);                          
+            
+                const statusEl = document.getElementById("p_fbInitialise");
+                if (statusEl) {
+                    statusEl.innerHTML = "Initialised";
+                }
+}
+
+function fb_authenticate(){
+    const AUTH = getAuth();
+    const PROVIDER = new GoogleAuthProvider();
+    PROVIDER.setCustomParameters({
+        prompt: 'select_account'
+    });
+    signInWithPopup(AUTH, PROVIDER).then((result) => {
+        console.log("working")
+    })
+    .catch((error) => {
+        console.log("not working")
+    });
+}
+
+/**************************************************************/
+// END OF CODE
+/**************************************************************/
